8e5d0673376deab4475cd3434a425027a69d4709
Uriah <codeitlikemiley@gmail.com>
2023-12-16 18:38:47 +0800

    updat main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,14 @@
-use {{service_name}}_service::{{service_name}}::{{service_name}}_service_server::{{project-name | pascal_case}}ServiceServer;
-use {{service_name}}_service::{{service_name}}_impl::{{project-name | pascal_case}}ServiceImpl;
+use {{service_name}}_service::{{service_name}}::{{service_name}}_service_server::{{service_name | pascal_case}}ServiceServer;
+use {{service_name}}_service::{{service_name}}_impl::{{service_name | pascal_case}}ServiceImpl;
 use tonic::transport::Server;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let addr = "[::1]:50051".parse()?;
-    let {{service_name}}_service = {{project-name | pascal_case}}ServiceImpl::default();
+    let {{service_name}}_service = {{service_name | pascal_case}}ServiceImpl::default();
 
     Server::builder()
-        .add_service({{project-name | pascal_case}}ServiceServer::new({{service_name}}_service))
+        .add_service({{service_name | pascal_case}}ServiceServer::new({{service_name}}_service))
         .serve(addr)
         .await?;
     Ok(())
