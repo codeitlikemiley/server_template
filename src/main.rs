ecb5fbedd4c147e4e627f55551eec98f93560e54
Uriah <codeitlikemiley@gmail.com>
2023-12-17 07:58:28 +0800

    simply api

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,15 @@
 use {{service_name}}_service::{{service_name}}::{{service_name}}_service_server::{{service_name | pascal_case}}ServiceServer;
 use {{service_name}}_service::{{service_name}}_impl::{{service_name | pascal_case}}ServiceImpl;
+use std::net::SocketAddr;
 use tonic::transport::Server;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let addr = "[::1]:50051".parse()?;
-    let {{service_name}}_service = {{service_name | pascal_case}}ServiceImpl::default();
+    let addr = SocketAddr::from(([0, 0, 0, 0], 50051));
 
     Server::builder()
-        .add_service({{service_name | pascal_case}}ServiceServer::new({{service_name}}_service))
+        .add_service({{service_name | pascal_case}}ServiceImpl::attach_reflection()?)
+        .add_service({{service_name | pascal_case}}ServiceImpl::new())
         .serve(addr)
         .await?;
     Ok(())
